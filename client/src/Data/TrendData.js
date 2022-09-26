8118cd1690487ee056aa0c10f90250986758ea04
Im-Koushik <koushiksurapaneni@gmail.com>
2022-09-26 19:32:46 +0530

    Social Media without Chat application

---
+++ client/src/Data/TrendData.js
@@ -0,0 +1,26 @@
+export const TrendData = [
+  {
+    name: "RRR",
+    shares: 97,
+  },
+  {
+    name: "Sachin",
+    shares: 80.5,
+  },
+  {
+    name: "MERN",
+    shares: 75.5,
+  },
+  {
+    name: "Fresher Job",
+    shares: 72,
+  },
+  {
+    name: "Har Ghar Iphone",
+    shares: 71.9,
+  },
+  {
+    name: "Openheimer",
+    shares: 20,
+  },
+];
